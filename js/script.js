8af179485e994085d44876ebcbc389892cd596a4
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-05-14 19:36:52 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-const sections = document.querySelectorAll('.js-scroll')
-const windowHeight = window.innerHeight * 0.4
+import initScrollAnimation from "./modules/scroll-animation.js"
 
-function animateScroll() {
-  sections.forEach(section => {
-    const sectionTop = section.getBoundingClientRect().top - windowHeight
-    const isSectionVisible = sectionTop - windowHeight < 0
-    if (isSectionVisible) {
-      section.classList.add('active')
-    } else {
-      section.classList.remove('active')
-    }
-  })
-}
-animateScroll()
-window.addEventListener('scroll', animateScroll)
+initScrollAnimation()
