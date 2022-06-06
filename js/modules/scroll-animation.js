c5f70d40632e43209e1ee849abeb8f19bb360e34
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-06-06 02:25:14 -0300

    Update scroll-animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -1,18 +1,38 @@
 export default function initScrollAnimation() {
   const sections = document.querySelectorAll('.js-scroll')
-  const windowHeight = window.innerHeight * 0.4
-  
-  function animateScroll() {
-    sections.forEach(section => {
-      const sectionTop = section.getBoundingClientRect().top - windowHeight
-      const isSectionVisible = sectionTop - windowHeight < 0
-      if (isSectionVisible) {
-        section.classList.add('active')
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active')
       } else {
-        section.classList.remove('active')
+        entry.target.classList.remove('active')
       }
     })
-  }
-  animateScroll()
-  window.addEventListener('scroll', animateScroll)
+  }, {
+    threshold: 0.3
+  })
+
+  sections.forEach(section => {
+    observer.observe(section)
+  })
+
+
+
+
+  // const sections = document.querySelectorAll('.js-scroll')
+  // const windowHeight = window.innerHeight * 0.4
+  
+  // function animateScroll() {
+  //   sections.forEach(section => {
+  //     const sectionTop = section.getBoundingClientRect().top - windowHeight
+  //     const isSectionVisible = sectionTop - windowHeight < 0
+  //     if (isSectionVisible) {
+  //       section.classList.add('active')
+  //     } else {
+  //       section.classList.remove('active')
+  //     }
+  //   })
+  // }
+  // animateScroll()
+  // window.addEventListener('scroll', animateScroll)
 }
